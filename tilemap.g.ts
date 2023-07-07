b29f8b8ad1a2648edbb8da171b6e9a59abf9daa9
Valis World <124701656+Valis-World@users.noreply.github.com>
2023-07-07 15:47:28 +0200

    Map design

---
+++ tilemap.g.ts
@@ -0,0 +1,14 @@
+// Automatisch generierter Code. Nicht bearbeiten.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Automatisch generierter Code. Nicht bearbeiten.
